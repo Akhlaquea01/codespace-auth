96d90ce7a6f27d91397c22fe8f66f34c49b87a2a
Akhlaque Ahmad <akhlaquea01@gmail.com>
2022-12-24 11:16:44 +0000

    Db Connected Successfully

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
+
+require("./database/database").connect();
 const express = require("express");
 
 const app = express();
-app.get("/",(req,res)=>{
-    res.send("<h1>Akhlaque</h1>")
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Akhlaque</h1>");
+});
 module.exports = app;
